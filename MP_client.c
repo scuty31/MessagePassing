dad360dfdbb9380939323afa5887ce1398fbdd66
scuty31 <hyunssi31@gmail.com>
2022-11-29 23:39:08 +0900

    messagePassing

--- MP_client.c
+++ MP_client.c
@@ -1,48 +1,49 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
 #include <ncurses.h>
+#include <signal.h>
+#include <errno.h>
 #include "mem.h"
 
-#define CONNECT 327687 +1 
-#define MAXOBN 
-
-typedef struct Messagetype{
-	long mtype;
-	char data[MAXOBN];
-	long source;
-}Message_t;
+#define CONNECT 32768+1 
 
 typedef struct DataMessage{
 	long mtype;
-	struct data_buf* data;
+	struct data_buf data;
+	long source;
 }Message_d;
 
+typedef struct MessageType{
+	long mtype;
+	char mtext [10];
+	long source;
+}Message_t;
+
 typedef struct MultipleArg{
-	int fd;
-	int id;
+	long fd;
+	long id;
 }MultipleArg;
 
+void initque();
 void initMenu();
 void initNcurses();
+void initMemoryData();
 void gameRoom();
 void waitingRoom();
+void sendConnection();
+void sendMessage();
+void recieveMessage();
 void* checkWaitingRoomPlayer2Status();
 void* checkGameRoomPlayer2TurnEnd();
-void sendConnectMessage();
-void* sendMessage(Message_t Message);
-void* sendData();
-void* receiveReady(void* arg);
-void* receiveMessage(void* arg);
 
 pthread_t waiting_thread, game_thread;
-data_buf* mem;
+data_buf mem;
 MultipleArg* mArg;
 
 char* waiting_status[] = {"Wait", "Join", "Ready"};
@@ -51,23 +52,29 @@ int quit = 0;
 WINDOW* waiting_player2_status;
 
 int main(){
-	mArg = (MultipleArg*)malloc(sizeof(MultipleArg));
-
-	mArg->fd = msgget((key_t)60109, IPC_CREAT | 0666);
-	if(mArg->fd == -1){
-		printf("error\n");
-		exit(0);
-	}
-	mArg->id = (long)getpid();
 
 	initMenu();
 
 	return 0;
 }
 
+
+void initque(){
+	mArg = (MultipleArg*)malloc(sizeof(MultipleArg));
+
+        mArg->fd = msgget((key_t)60109, IPC_CREAT | 0666);
+        if(mArg->fd == -1){
+                printf("error\n");
+                exit(0);
+        }
+        mArg->id = (long)getpid();
+}
+
 void initMenu(){
 	initNcurses();
-	
+	initque();
+	initMemoryData();
+
 	char* select[] = {"Game Strat", "Exit"};
 
 	int xStart = 8, yStart = 4;
@@ -117,7 +124,7 @@ void initMenu(){
 	}
 
 	if(highlight == 0){
-		sendConnectMessage();
+		sendConnection();
 		waitingRoom();
 	}
 	else{
@@ -134,6 +141,21 @@ void initNcurses(){
 	curs_set(0);
 }
 
+void initMemoryData(){
+	mem.wait_msg.connect = 0;
+	mem.wait_msg.ready = 0;
+	mem.wait_msg.status_change = 0;
+	mem.wait_msg.opponent_connect = 0;
+	mem.wait_msg.opponent_ready = 0;
+	mem.wait_msg.opponent_change = 0;
+
+	mem.game_msg.my_turn = 0;
+	mem.game_msg.result = 0;
+	mem.game_msg.row = 0;
+	mem.game_msg.col = 0;
+	mem.game_msg.turn_end = 0;
+}
+
 void waitingRoom(){
 	initNcurses();
 
@@ -142,11 +164,10 @@ void waitingRoom(){
 
 	char* select[] = {"Ready!!", "Exit"};
 	
-	mem->wait_msg.connect = 1;
-	mem->wait_msg.status_change = 1;
-	clock_gettime(CLOCK_MONOTONIC, &mem->start);
+	mem.wait_msg.connect = 1;
+	mem.wait_msg.status_change = 1;
 	
-	sendData(); // wait_msg.connect = 1	
+	sendMessage(); // wait_msg.connect = 1	
 
 	WINDOW* player1 = newwin(5, 15, yStart, xStart);
         box(player1, 0, 0);
@@ -224,72 +245,215 @@ void waitingRoom(){
 	        mvwprintw(player1_status, 0, 0, "%s", waiting_status[2]);
 	        wrefresh(player1_status);
 		
-		mem->wait_msg.ready = 1;
-		mem->wait_msg.status_change = 1;
+		mem.wait_msg.ready = 1;
+		mem.wait_msg.status_change = 1;
 	
-		sendData();
-		
+		sendMessage();
+
 		pthread_join(waiting_thread, NULL);
+
+		WINDOW* start = newwin(5, 30, yStart + 5, xStart);
+		for(int i = 0; i < 3; i++){
+			mvwprintw(start, 0, 0, "Start in %d seconds!!", 3 - i);
+			wrefresh(start);
+			sleep(1);
+		}
+		
+		endwin();
+
+		//gameRoom();
 	}
 
+	else{
+		mem.wait_msg.connect = 0;
+		mem.wait_msg.ready = 0;
+		mem.wait_msg.status_change = 0;
+		mem.wait_msg.opponent_connect = 0;
+		mem.wait_msg.opponent_ready = 0;
+		mem.wait_msg.opponent_change = 0;
+		sendMessage();
+
+		endwin();
+		return;
+	}
 }
 
-void sendConnectMessage(){
+void sendConnection(){
 	Message_t message;
 	message.mtype = CONNECT;
 	message.source = mArg->id;
-	msgsnd(mArg->fd, &message, sizeof(message) - sizeof(long), 0);
+
+	if(msgsnd(mArg->fd, &message, sizeof(message) - sizeof(long), 0))
+                        perror("msgsnd failed");
 }
 
-void* sendMessage(Message_t Message){
-
-	return NULL;
-}
-
-void* sendData(){
+void sendMessage(){
 	Message_d message;
-	message.mtype = CONNECT;
+	message.mtype = 1;
 	message.data = mem;
-	msgsnd(mArg->fd, &message, sizeof(message) - sizeof(long), 0);
+	message.source = mArg->id;
+
+	if(msgsnd(mArg->fd, &message, sizeof(message) - sizeof(long), 0))
+			perror("msgsnd failed");
+			
 }
 
 void recieveData(){
 	Message_d message;
-	msgrcv(mArg->fd, &message, sizeof(message) - sizeof(long), mArg->id, 0);
+	if((msgrcv(mArg->fd, &message, sizeof(message) - sizeof(long), mArg->id, 0))==-1)
+		perror("msgrcv failed");
 	mem = message.data;
 }
 
 void* checkWaitingRoomPlayer2Status(){
-	
-
 	mvwprintw(waiting_player2_status, 0, 0, "%s", waiting_status[0]);
         wrefresh(waiting_player2_status);
 	
 	// player2의 상태가 ready가 될 때 까지 반복
-	while(mem->wait_msg.opponent_ready == 0){
+	while(mem.wait_msg.opponent_ready == 0){
 		recieveData();
 		// player2가 연결이 안되어있으면 wait 상태 표시
-                if(mem->wait_msg.opponent_connect == 1){
+                if(mem.wait_msg.opponent_connect == 1){
                 	touchwin(waiting_player2_status);
                         mvwprintw(waiting_player2_status, 0, 0, "%s", waiting_status[0]);
                         wrefresh(waiting_player2_status);
                 }
 		// player2가 연결되었으면 join 상태 표시
-                else if(mem->wait_msg.opponent_ready == 1){
+                else if(mem.wait_msg.opponent_ready == 1){
                 	touchwin(waiting_player2_status);
                         mvwprintw(waiting_player2_status, 0, 0, "%s", waiting_status[1]);
                         wrefresh(waiting_player2_status);
                 }
 			// player2가 준비를 하면 ready 상태 표시
-                if(mem->wait_msg.opponent_ready == 1){
+                if(mem.wait_msg.opponent_ready == 1){
                 	touchwin(waiting_player2_status);
                         mvwprintw(waiting_player2_status, 0, 0, "%s", waiting_status[2]);
                         wrefresh(waiting_player2_status);
                 }
+		
 	}
 	// 쓰레드 종료
 	pthread_exit(NULL);
 	
 }
 
+void gameRoom(){
+	initNcurses();
 
+	int xStart = 5, yStart = 3;
+	int i, k, xPoint = 3;
+	int row = 0, column = 0;
+
+	move(yStart, xStart + 2);
+	keypad(stdscr, TRUE);
+
+	refresh();
+
+	while(1){
+		pthread_create(&game_thread, NULL, checkGameRoomPlayer2TurnEnd, NULL);
+		pthread_join(game_thread, NULL);
+
+		if(mem.game_msg.result==2){
+			mvprintw(yStart + 15, xStart, "Lose...");
+			refresh();
+			for(int i = 0; i< 3; i++){
+				mvprintw(yStart + 15 + 1, xStart, "End in %d seconds", 3 - i);
+				refresh();
+				sleep(1);
+			}
+			endwin();
+			return;
+		}
+
+		attron(A_REVERSE);
+		mvprintw(row + yStart, xStart + column * xPoint + 2, "%c", mem.game_msg.omok_board[row][column]);
+		attroff(A_REVERSE);
+
+		int c;
+		c = getch();
+
+		switch(c){
+			case KEY_UP:
+				if(row == 0)
+					row = 14;
+				else
+					row--;
+				break;
+			case KEY_DOWN:
+				if(row == 14)
+					row = 0;
+				else
+					row++;
+				break;
+			case KEY_LEFT:
+				if(column == 0)
+					column = 14;
+				else
+					column--;
+				break;
+			case KEY_RIGHT:
+				if(column == 14)
+					column = 0;
+				else
+					column++;
+				break;
+			default:
+				break;
+		}
+
+		if(c == 10|| c == ' '){
+			if(mem.game_msg.my_turn == 1 && mem.game_msg.omok_board[row][column] == '+'){
+				mem.game_msg.omok_board[row][column]='O';
+				mem.game_msg.turn_end = 1;
+				mem.game_msg.row = row;
+				mem.game_msg.col = column;
+				
+				usleep(10000);
+
+				if(mem.game_msg.result == 1){
+					mvprintw(yStart + 15, xStart, "Win!!");
+					refresh();
+
+					for(int i = 0; i<3; i++){
+						mvprintw(yStart + 15 + 1, xStart, "End in %d seconds", 3-i);
+						refresh();
+						sleep(1);
+					}
+					endwin();
+					return;
+				}
+			}
+		}
+	}
+	endwin();
+}
+
+void* checkGameRoomPlayer2TurnEnd(){
+	int xPoint = 3, xStart = 5, yStart = 3;
+	int i, k;
+
+	for(i = 0; i<ROW; i++){
+		mvprintw(i + yStart, xStart, "|");
+		for(k = 0; k < COLUMN; k++){
+			mvprintw(i + yStart,  1 + xPoint * k + xStart, "-%c-", mem.game_msg.omok_board[i][k]);
+		}
+		mvprintw(i + yStart, 1 + xPoint * 15 + xStart, "|");
+	}
+
+	refresh();
+
+	while(1){
+		if(mem.game_msg.my_turn != 0){
+			for(i = 0; i<ROW; i++){
+				mvprintw(i + yStart, xStart, "|");
+				for(k=0; k<COLUMN; k++){
+					mvprintw(i + yStart, 1 + xPoint * k + xStart, "-%c-", mem.game_msg.omok_board[i][k]);
+				}
+				mvprintw(i + yStart, 1 + xPoint * 15 + xStart, "|");
+			}
+			refresh();
+
+			break;
+		}
+	}
+ }
--- MP_server.c
+++ MP_server.c
@@ -2,63 +2,59 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <ncurses.h>
+#include <signal.h>
 #include <pthread.h>
 #include <sys/msg.h>
-#include <fcntl.h>
-#include "mem.h"
 #include <sys/types.h>
 #include <sys/ipc.h>
+#include <errno.h>
+#include "mem.h"
 
-#define MAXOBN 1024 
-#define CONNECT 327687+1
-
-typedef struct MessageType{
-	long mtype;
-	char data[MAXOBN];
-	long source;
-}Message_t;
+#define CONNECT 32768+1
 
 typedef struct DataMessage{
 	long mtype;
-	struct data_buf* data;
+	struct data_buf data;
+	long source;
 }Message_d;
 
 typedef struct Room{
 	long clients[2];
 }Room;
 
-data_buf* mem1;
-data_buf* mem2;
-data_buf* mem;
-Room* room;
-key_t mqid;
 
+data_buf mem1;
+data_buf mem2;
+data_buf mem;
+Room room;
+int mqid;
+
+int user;
+
+void initque();
 void initMemoryData();
 void recieveConnection();
+void sendMessageUser1();
+void sendMessageUser2();
+void recieveData();
 void* waitingRoomDataCommunication();
 void* gameRoomDataCommunication();
-void initque();
 void* judgeOmok(void* turn);
-void setReadydata();
 
 int main(){
-	room->clients[0] = room->clients[1] = 0;
-
 	pthread_t waiting_thread;
 	pthread_t game_thread;
 	
 	initque();
 	initMemoryData();
-	recieveConnection();	
-
-	pthread_create(&waiting_thread, NULL, waitingRoomDataCommunication, NULL);
 	
-	recieveConnection();
-
+	printf("a\n");	
+	pthread_create(&waiting_thread, NULL, waitingRoomDataCommunication, NULL);
 	pthread_join(waiting_thread, NULL);
 	
-	//pthread_create(&game_thread, NULL, gameRoomDataCommunication, NULL);
-        //pthread_join(game_thread, NULL);
+	pthread_create(&game_thread, NULL, gameRoomDataCommunication, NULL);
+        pthread_join(game_thread, NULL);
 
 	sleep(5);
 
@@ -73,101 +69,152 @@ void initque(){
 }
 
 void initMemoryData(){
-	mem1->wait_msg.connect = 0;
-	mem1->wait_msg.ready = 0;
-	mem1->wait_msg.status_change = 0;
-	mem1->wait_msg.opponent_connect = 0;
-	mem1->wait_msg.opponent_ready = 0;
-	mem1->wait_msg.opponent_change = 0;
+	mem1.wait_msg.connect = 0;
+	mem1.wait_msg.ready = 0;
+	mem1.wait_msg.status_change = 0;
+	mem1.wait_msg.opponent_connect = 0;
+	mem1.wait_msg.opponent_ready = 0;
+	mem1.wait_msg.opponent_change = 0;
 
-	mem1->game_msg.my_turn = 0;
-	mem1->game_msg.result = 0;
-	mem1->game_msg.row = 0;
-	mem1->game_msg.col = 0;
-	mem1->game_msg.turn_end = 0;
+	mem1.game_msg.my_turn = 0;
+	mem1.game_msg.result = 0;
+	mem1.game_msg.row = 0;
+	mem1.game_msg.col = 0;
+	mem1.game_msg.turn_end = 0;
 
-	mem2->wait_msg.connect = 0;
-        mem2->wait_msg.ready = 0;
-        mem2->wait_msg.status_change = 0;
-        mem2->wait_msg.opponent_connect = 0;
-        mem2->wait_msg.opponent_ready = 0;
-        mem2->wait_msg.opponent_change = 0;
+	mem2.wait_msg.connect = 0;
+        mem2.wait_msg.ready = 0;
+        mem2.wait_msg.status_change = 0;
+        mem2.wait_msg.opponent_connect = 0;
+        mem2.wait_msg.opponent_ready = 0;
+        mem2.wait_msg.opponent_change = 0;
 
-	mem2->game_msg.my_turn = 0;
-        mem2->game_msg.result = 0;
-	mem2->game_msg.row = 0;
-        mem2->game_msg.col = 0;
-        mem2->game_msg.turn_end = 0;
+	mem2.game_msg.my_turn = 0;
+        mem2.game_msg.result = 0;
+	mem2.game_msg.row = 0;
+        mem2.game_msg.col = 0;
+        mem2.game_msg.turn_end = 0;
 
 	for(int i = 0; i < ROW; i++){
 		for(int k = 0; k < COLUMN; k++){
-			mem1->game_msg.omok_board[i][k] = '+';
-			mem2->game_msg.omok_board[i][k] = '+';
+			mem1.game_msg.omok_board[i][k] = '+';
+			mem2.game_msg.omok_board[i][k] = '+';
 		}
 	}
 }
 
 void recieveConnection(){
-	Message_t message;
-	if((msgrcv(mqid, &message, sizeof(message) - sizeof(long), CONNECT, 0))<0){
-		printf("error\n");
+	Message_d message;
+	int i;
+	for(i=0; i<2; i++){	
+		if((msgrcv(mqid, &message, sizeof(message) - sizeof(long), CONNECT, 0)) < 0)
+			perror("msgrcv failed");
+		else{
+			room.clients[i] = message.source;
+			printf("player %ld connect\n", room.clients[i]);
+		}
 	}
-	else{
-		if(room->clients[0]==0)
-			room->clients[0] = message.source;
-		else
-			room->clients[1] = message.source;
-	}
+}
+
+void sendMessageUser1(){
+	Message_d message;
+	message.mtype = room.clients[0];
+	message.data = mem1;
+	
+	if(msgsnd(mqid, &message, sizeof(message) - sizeof(long), 0))	
+		perror("msgsnd failed");
+}
+
+void sendMessageUser2(){
+	Message_d message;
+        message.mtype = room.clients[1];
+        message.data = mem2;
+
+	if(msgsnd(mqid, &message, sizeof(message) - sizeof(long), 0))
+              	perror("msgsnd failed");
 }
 
 void recieveData(){
 	Message_d message;
 	if((msgrcv(mqid, &message, sizeof(message) - sizeof(long), CONNECT, 0))<0){
-		printf("error\n");
+		perror("msgrcv failed");
 	}
 	else{
 		mem = message.data;
+		if(message.source == room.clients[0])
+			mem1 = message.data;	
+		else if(message.source == room.clients[1])
+			mem2 = message.data;
+		else
+			perror("msgrcv source error");
 	}
-}
-
-void setReadydata(){
-	mem1->wait_msg.opponent_connect = mem2->wait_msg.connect;
-        mem1->wait_msg.opponent_ready = mem2->wait_msg.ready;
-        mem1->wait_msg.opponent_change = mem2->wait_msg.status_change;
-	
-	mem2->wait_msg.opponent_connect = mem1->wait_msg.connect;
-        mem2->wait_msg.opponent_ready = mem1->wait_msg.ready;
-        mem2->wait_msg.opponent_change = mem1->wait_msg.status_change;
-
 }
 
 void* waitingRoomDataCommunication(){
 	int player1_ready = 0;
 	int player2_ready = 0;
-	double accum;
+	recieveConnection();
+	
+	printf("c\n"); 
 	while(player1_ready == 0 || player2_ready == 0){
-		
-		if(mem1->wait_msg.status_change == 1){
-			if(mem1->wait_msg.connect == 1){
-				mem2->wait_msg.opponent_change = 1;
-				mem2->wait_msg.opponent_connect = 1;
-				setReadydata();
+		recieveData();
+		if(user == 1){
+			
+			if(mem1.wait_msg.connect == 1){
+				mem2.wait_msg.opponent_change = 1;
+				mem2.wait_msg.opponent_connect = 1;
 			}
-			else if(mem1->wait_msg.connect == 0){
-				mem2->wait_msg.opponent_change = 1;
-				mem2->wait_msg.opponent_connect = 0;
-				setReadydata();
+			else if(mem1.wait_msg.connect == 0){
+				mem2.wait_msg.opponent_change = 1;
+				mem2.wait_msg.opponent_connect = 0;
 			}
-			if(player1_ready == 0 && mem1->wait_msg.ready == 1){
-				mem2->wait_msg.opponent_change = 1;
-				mem2->wait_msg.opponent_ready = 1;
-				setReadydata();
+			
+			if(player1_ready == 0 && mem1.wait_msg.ready == 1){
+				mem2.wait_msg.opponent_change = 1;
+				mem2.wait_msg.opponent_ready = 1;
+
+				player1_ready=1;
+			}
+			mem1.wait_msg.status_change = 0;
+			sendMessageUser1();
+			sendMessageUser2();
+		}
+
+		if(user == 2){
+			if(mem2.wait_msg.connect == 1){
+				mem1.wait_msg.opponent_change = 1;
+				mem1.wait_msg.opponent_connect = 1;
+			}
+			else if(mem2.wait_msg.connect == 0){
+				mem1.wait_msg.opponent_change = 1;
+				mem1.wait_msg.opponent_connect = 0;
+			}
+
+			if(player2_ready == 0 && mem2.wait_msg.ready == 1){
+				mem1.wait_msg.opponent_change = 1;
+				mem1.wait_msg.opponent_ready=1;
 
 				player2_ready = 1;
 			}
-			mem2->wait_msg.status_change = 0;
+			mem2.wait_msg.status_change = 0;
+			sendMessageUser2();
+			sendMessageUser1();
 		}
+
 	}
 
 	pthread_exit(NULL);
+}
+
+void* gameRoomDataCommunication(){
+	void* ret;
+	int turn;
+	double accum;
+	pthread_t judge_thread;
+	
+	mem1.game_msg.my_turn = 1;
+	
+	while(1){
+		while(1){
+
 }
